d5c901f0b28625e21fb71b50f85171a3142bdd91
Sathish Krishnan <77665129+sathishkrishnanyara@users.noreply.github.com>
2025-09-18 18:39:29 +0530

    Update debug.js

--- src/debug.js
+++ src/debug.js
@@ -1,14 +1,17 @@
 
 function createDebug () {
+  
   try {
     if ((typeof process !== 'undefined' && process.env && process.env.DEBUG && process.env.DEBUG.match(/json-rules-engine/)) ||
         (typeof window !== 'undefined' && window.localStorage && window.localStorage.debug && window.localStorage.debug.match(/json-rules-engine/))) {
       return console.debug.bind(console)
     }
+    return console.debug.bind(console)
   } catch (ex) {
     // Do nothing
+    return console.debug.bind(console)
   }
-  return () => {}
+  return console.debug.bind(console)
 }
 
 export default createDebug()
